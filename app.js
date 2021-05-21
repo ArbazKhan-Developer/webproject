80e3d91e269091f6b4b554b8d9d36e0843b4115c
ak-anonymous <83087214+ak-anonymous@users.noreply.github.com>
2021-05-21 17:21:16 +0530

    node js project first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+ const express = require("express");
+ const app = express();
+ const path = require('path');
+ const fs = require("fs");
+ const port = 80;
+
+ app.use('/static', express.static('static'));
+ app.use(express.urlencoded())
+
+ app.set('view engine', 'pug');
+app.set('views',path.join(__dirname, 'views'));
+
+
+ app.get('/',(eq,res)=>{
+     res.status(200).render('index.pug')
+ })
+ app.post('/',(req,res)=>{
+     Name= (req.body).name
+     Email= (req.body).email
+     Phone= (req.body).phone
+     Massege= (req.body).massege
+    
+
+     let output = `name of client: ${Name} ,email of clients: ${Email} ,phone number of client: ${Phone} ,massege of client: ${Massege} `
+
+     fs.writeFileSync('form.txt',output);
+     const params= {'massage':'your form have been submitted successfully'};
+    res.status(200).render('index.pug',params)
+ })
+
+ app.listen(port, () =>{
+    console.log(`server is running on port:${port}`);
+ })
---
+++ static/food.js
@@ -0,0 +1,6 @@
+function submit(){
+    document.getElementById('btn')
+    document.write('form have submitted succelssfuly')
+
+    console.log('form have submitted succelssfuly');
+}
